91ef96c8dfb38d75f5b1503b0f81f4fc4a748d57
Nuha Sahraoui <nuha.sahraoui@gmail.com>
2020-07-25 08:57:48 -0400

    updated lexer for keywords, variables and other tokens

--- lexer.cpp
+++ lexer.cpp
@@ -17,6 +17,15 @@
 #define RPAR "RPAR"
 #define OP "OP"
 #define INT "INT"
+#define OP "OP"
+#define INT "INT"
+#define BEGIN "BEGIN"
+#define END "END"
+#define DOT "DOT"
+#define ASSIGN "ASSSIGN"
+#define SEMI "SEMI"
+#define ID "ID"
+
 
 std::ostream& lexer::operator<< (std::ostream& out, const lexer::Token& token)
 {
@@ -27,6 +36,7 @@ std::ostream& lexer::operator<< (std::ostream& out, const lexer::Token& token)
 lexer::Token::Token(std::string t, char v) {
     type = t;
     value = v;
+    str = nullptr;
 }
 
 lexer::Lexer::Lexer(std::string scr) {
@@ -38,6 +48,14 @@ lexer::Lexer::Lexer(std::string scr) {
 void lexer::Lexer::error() {
     std::cout << "Error parsing input" << "\n";
     throw;
+}
+
+char lexer::Lexer::peek() {
+    int peek_pos = position + 1;
+    if (peek_pos > script.size() - 1)
+        return 0;
+    else
+        return script[peek_pos];
 }
 
 void lexer::Lexer::skip_whitespace() {
@@ -72,11 +90,57 @@ int lexer::Lexer::to_integer(std::string number) {
     return result;
 }
 
+lexer::Token* lexer::Lexer::keyword() {
+    std::string keyword = "";
+    while (curr_character != NULL && isdigit(curr_character)) {
+        keyword.push_back(curr_character);
+        advance();
+    }
+    if (keyword.size() == 1) {
+        for (int i = 0; i < reserved_keywords.size(); i++) {
+            if (keyword[0] == reserved_keywords[i]->value)
+                return reserved_keywords[i];
+        }
+    }
+    else if (keyword.size() > 1) {
+        for (int i = 0; i < reserved_keywords.size(); i++) {
+            if (!(keyword.compare(reserved_keywords[i]->str)))
+                return reserved_keywords[i];
+        }
+    }
+    if (keyword.size() == 1) {
+        lexer::Token* token = new Token(ID, keyword[0]);
+        return token;
+    }
+    else {
+        lexer::Token* token = new Token(ID, 0);
+        token->str = keyword;
+        return token;
+    }
+}
+
 lexer::Token* lexer::Lexer::next_token() {
     while (curr_character != NULL) {
 
         skip_whitespace();
 
+        if (curr_character == ':' && lexer::Lexer::peek() == '=') {
+            Token* token = new Token(SEMI, 0);
+            token->str = ":=";
+            advance();
+            advance();
+            return token;
+        }
+        if (curr_character == ';') {
+            Token* token = new Token(SEMI, curr_character);
+            advance();
+            return token;
+        }
+        if (curr_character == '.') {
+            Token* token = new Token(DOT, curr_character);
+            advance();
+            return token;
+        }
         if (curr_character == '+') {
             Token* token = new Token(PLUS, curr_character);
             advance();
--- lexer.h
+++ lexer.h
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 namespace lexer {
 
     class Token {
     public:
         char value;
+        std::string str;
         std::string type;
         Token(std::string t, char v);
         friend std::ostream& operator<< (std::ostream& out, const Token& token);
@@ -18,14 +20,17 @@ namespace lexer {
         int position;
         std::string script;
         char curr_character;
+        std::vector<Token *> reserved_keywords;
 
         Lexer(std::string scr);
         void error();
+        char peek();
         void skip_whitespace();
         void advance();
         int integer();
         int to_integer(std::string number);
         Token* next_token();
+        Token* keyword();
         //friend parser::Parser; //magic line of code that somehow solves all my problems
         
     };
